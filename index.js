6e93384a8308418bbc3881c49f4fd77c46585904
omar <thejarvisgroupllc@gmail.com>
2021-05-17 16:04:02 -0400

    commit tree a77ebe0f7068c743aac47a6dbed95d3de86afe01

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+  ];
 
+function superBowlWin() {
+    return record.win === "W";
+}
+
+console.log(record.find(superBowlWin));
